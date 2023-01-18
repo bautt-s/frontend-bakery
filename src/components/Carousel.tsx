d0e7c1d5421eb3418fab2f51c10683da49faee8f
bautt-s <bauti_hora@hotmail.com>
2023-01-18 01:18:45 -0300

    top presentation done

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -15,19 +15,33 @@ const Carousel = () => {
                         We deliver the tastiest food, right from the oven, and directly to your door.
                     </p>
 
-                    <button className="flex flex-row mt-[40px]">
-                        <span className="text-2xl">Discover</span>
+                    <button className="flex flex-row mt-[40px] rounded-md border px-5 pt-1 pb-2 hover:bg-[#ffffff18]">
+                        <span className="text-2xl font-semibold">Discover</span>
                     </button>
                 </div>
             </div>
 
             <div className='flex flex-col w-1/3'>
-                <div className='w-full bg-[#8f8a81]'>
+                <div className='w-full bg-[#8f8a81] flex items-center text-white'>
                     <img src={secondPicture} className="object-cover w-full h-[460px] opacity-70" />
+                    <div className="absolute w-1/3 flex flex-col items-center">
+                        <h1 className='text-4xl font-bold text-center mt-5'>TOP CHOICES 2023</h1>
+
+                        <button className="flex flex-row mt-[10px] rounded-md border border-transparent hover:border-white px-5 pt-1 pb-2">
+                            <span className="text-2xl font-semibold">Learn more</span>
+                        </button>
+                    </div>
                 </div>
 
-                <div className="bg-[#8f8a81]">
-                    <img src={thirdPicture} className="object-cover h-auto opacity-70" />
+                <div className='w-full bg-[#8f8a81] flex items-center text-white'>
+                    <img src={thirdPicture} className="object-cover w-full h-auto opacity-70" />
+                    <div className="absolute w-1/3 flex flex-col items-center">
+                        <h1 className='text-4xl font-bold text-center mt-5'>BAKERY MENU</h1>
+
+                        <button className="flex flex-row mt-[10px] rounded-md border border-transparent hover:border-white px-5 pt-1 pb-2">
+                            <span className="text-2xl font-semibold">Take a peek</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
